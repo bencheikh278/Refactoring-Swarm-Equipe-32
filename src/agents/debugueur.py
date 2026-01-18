76822da8dccfcd45df32c5a1c99125b15247eec7
Bencheikh ouafa <no_bencheikh@esi.dz>
2026-01-18 11:37:42 +0100

    ..

--- src/agents/debugueur.py
+++ src/agents/debugueur.py
@@ -1,9 +1,3 @@
 """Agent Testeur"""
 class TesterAgent:
-    def __init__(self):
-        pass
-    
-    def test(self, target_dir):
-        print(f"   Tests dans {target_dir}")
-        # on doit ajout les methode pour gerer les test 
-        return {'passed': True, 'errors': []}
+  
